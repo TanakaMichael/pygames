d06e6a4e21ba341a7fcb59a3a6d09ff3a6f2548e
TanakaMichael <yuusya12411@outlook.jp>
2025-02-02 00:24:35 +0900

    a

--- core/network/network_component.py
+++ core/network/network_component.py
@@ -3,29 +3,72 @@ from core.component.component import Component
 from core.network.network_manager import NetworkManager
 
 class NetworkComponent(Component):
-    """ネットワーク同期可能なコンポーネントの基底クラス"""
+    """ネットワーク同期可能なコンポーネントの基底クラス
+    ・同期対象の変数を「キー」と「対象オブジェクト＋属性名」のペアとして登録
+    ・update() 時に各変数の現在値と前回値を比較し、変化があった場合のみ差分をブロードキャスト
+    ・受信した差分データは自動的に対象の属性に書き込み
+    """
     def __init__(self, game_object, sync_interval=0.05):
         super().__init__(game_object)
         self.network_manager = NetworkManager.get_instance()
         self.network_id = game_object.network_id  # オブジェクトの Network ID
         self.sync_interval = sync_interval          # 同期間隔 (秒)
         self.time_since_last_sync = 0               # 前回の同期経過時間
-        self.prev_state = self._get_current_state()  # 前回送信した Transform 状態
+        # 同期対象変数の辞書： key -> (target_object, attribute_name, last_value)
+        self._sync_vars = {}
+
+    def register_sync_variable(self, key, target, attr_name):
+        """同期対象の変数を登録する
+        :param key: 同期時に使用するキー文字列
+        :param target: 変数が存在する対象オブジェクト（例：game_object.transform）
+        :param attr_name: 対象の属性名（例："x"）
+        """
+        initial_value = getattr(target, attr_name)
+        self._sync_vars[key] = (target, attr_name, initial_value)
+
     def update(self, delta_time):
-        """サーバーなら、一定時間経過後に状態変化があれば同期メッセージを送信"""
+        """サーバー側なら、一定時間経過後に各同期対象変数の値をチェックし、変化があれば差分をブロードキャストする"""
         if self.network_manager.is_server:
             self.time_since_last_sync += delta_time
             if self.time_since_last_sync >= self.sync_interval:
-                current_state = self._get_current_state()
-                if current_state != self.prev_state:
-                    self.on_sync_broadcast()
-                    self.prev_state = current_state
+                diff = {}
+                for key, (target, attr, last_val) in self._sync_vars.items():
+                    current_val = getattr(target, attr)
+                    if current_val != last_val:
+                        diff[key] = current_val
+                        self._sync_vars[key] = (target, attr, current_val)
+                if diff:
+                    self.on_sync_broadcast(diff)
                 self.time_since_last_sync = 0
-    def on_sync_broadcast(self):
-        pass
-    def handle_network_data(self, data):
-        pass
-    def _get_current_state(self):
-        pass
+
+    def on_sync_broadcast(self, diff):
+        """差分データ diff をブロードキャストする
+        デフォルトでは、"sync_update" タイプのメッセージとして送信
+        """
+        data = {"type": "sync_update", "network_id": self.network_id}
+        data.update(diff)
+        self.network_manager.broadcast(data)
+
     def force_broadcast(self):
-        pass
+        """強制的に全ての同期対象変数の値をブロードキャストする"""
+        diff = {}
+        for key, (target, attr, _) in self._sync_vars.items():
+            current_val = getattr(target, attr)
+            diff[key] = current_val
+            self._sync_vars[key] = (target, attr, current_val)
+        self.on_sync_broadcast(diff)
+
+    def handle_network_data(self, data):
+        """受信側で自動的に同期対象変数を更新する。
+        送信側からの差分メッセージに含まれるキーと一致する変数に値を書き込みます。
+        """
+        if data.get("type") != "sync_update" or data.get("network_id") != self.network_id:
+            return
+        # data に含まれるキーのうち、"type" と "network_id" 以外を更新
+        for key, value in data.items():
+            if key in ("type", "network_id"):
+                continue
+            if key in self._sync_vars:
+                target, attr, _ = self._sync_vars[key]
+                setattr(target, attr, value)
+                self._sync_vars[key] = (target, attr, value)
--- core/network/network_transform.py
+++ core/network/network_transform.py
@@ -6,45 +6,21 @@ class NetworkTransform(NetworkComponent):
     def __init__(self, game_object, sync_interval=0.05):
         super().__init__(game_object, sync_interval)
         self.network_manager = NetworkManager.get_instance()
+        # 例えば、game_object.transform は Transform オブジェクトとする
+        self.register_sync_variable("position_x", self.game_object.transform.position, "x")
+        self.register_sync_variable("position_y", self.game_object.transform.position, "y")
+        self.register_sync_variable("scale_x", self.game_object.transform.scale, "x")
+        self.register_sync_variable("scale_y", self.game_object.transform.scale, "y")
+        self.register_sync_variable("rotation_x", self.game_object.transform.rotation, "x")
+        self.register_sync_variable("rotation_y", self.game_object.transform.rotation, "y")
+        self.register_sync_variable("rotation_z", self.game_object.transform.rotation, "z")
 
     def update(self, delta_time):
         """サーバーなら Transform をブロードキャスト"""
         super().update(delta_time)
-
-    def broadcast_transform(self):
-        """サーバーが Transform をクライアントに送信"""
-        transform_data = {
-            "type": "transform_update", 
-            "network_id": self.network_id,
-            "position_x": self.game_object.transform.position.x,
-            "position_y": self.game_object.transform.position.y,
-            "scale_x": self.game_object.transform.scale.x,
-            "scale_y": self.game_object.transform.scale.y,
-            "rotation_x": self.game_object.transform.rotation.x,
-            "rotation_y": self.game_object.transform.rotation.y,
-            "rotation_z": self.game_object.transform.rotation.z,
-        }
-        self.network_manager.broadcast(transform_data)
-    def _get_current_state(self):
-        """現在の Transform の状態を辞書として取得"""
-        return {
-            "position_x": self.game_object.transform.position.x,
-            "position_y": self.game_object.transform.position.y,
-            "scale_x": self.game_object.transform.scale.x,
-            "scale_y": self.game_object.transform.scale.y,
-            "rotation_x": self.game_object.transform.rotation.x,
-            "rotation_y": self.game_object.transform.rotation.y,
-            "rotation_z": self.game_object.transform.rotation.z,
-        }
-    def on_sync_broadcast(self): # 定期イベント
-        super().on_sync_broadcast()
-        """サーバーが Transform 状態をクライアントに送信"""
-        transform_data = {
-            "type": "transform_update",
-            "network_id": self.network_id,
-            **self._get_current_state()
-        }
-        self.network_manager.broadcast(transform_data)
+    def on_sync_broadcast(self, diff): # 定期イベント
+        """変数を同期します"""
+        super().on_sync_broadcast(diff)
     def force_broadcast(self):
         """強制的に同期メッセージを送信するメソッド
         ※シーン更新完了時など、全ネットワークコンポーネントの最新状態をクライアントに送信する際に使用
@@ -53,7 +29,5 @@ class NetworkTransform(NetworkComponent):
         self.prev_state = self._get_current_state()
     def handle_network_data(self, data):
         """受信データが Transform 更新かチェックして適用"""
-        if data.get("type") == "transform_update" and data.get("network_id") == self.network_id:
-            self.game_object.transform.position = pygame.Vector2(data["position_x"], data["position_y"])
-            self.game_object.transform.scale = pygame.Vector2(data["scale_x"], data["scale_y"])
-            self.game_object.transform.rotation = pygame.Vector3(data["rotation_x"], data["rotation_y"], data["rotation_z"])
+        super().handle_network_data(data)
+        
